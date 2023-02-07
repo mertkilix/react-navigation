86b9e0871d55ce9b86542c2c704dd71270324866
mertkilix <mertkilic45@hotmail.com>
2023-02-06 22:41:54 -0800

    navigation exercise

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MenuScreen from "./Screens/MenuScreen";
+import WelcomeScreen from "./Screens/WelcomeScreen";
+const Stack = createNativeStackNavigator();
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <NavigationContainer>
+      <Stack.Navigator 
+      initialRouteName="MenuScreen" 
+      screenOptions={{ 
+        headerStyle: { backgroundColor: '#333333'},
+        headerTintColor:'#fff',
+        headerTitleStyle:{ fontWeight:'bold'}
+      }}>
+      <Stack.Screen name="MenuScreen"    options={{ title: 'Login' }}
+ component={MenuScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffff',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ Screens/WelcomeScreen.js
@@ -0,0 +1,79 @@
+import { ScrollView, View, Text, StyleSheet, Image, Pressable } from 'react-native';
+
+
+export default WelcomeScreen = ({navigation}) => {
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.headerWrapper}>
+        <Image
+          style={styles.image}
+          source={require('./img/logo.png')}
+          resizeMode="cover"
+          accessible={true}
+          accessibilityLabel={'Little Lemon Logo'}
+        />
+
+        <Text style={styles.headerText}>Little Lemon</Text>
+      </View>
+      <Text style={styles.regularText}>
+        Little Lemon is a charming neighborhood bistro that serves simple food
+        and classic cocktails in a lively but casual environment. We would love
+        to hear your experience with us!
+      </Text>
+
+            <Pressable onPress={() => navigation.goBack() } style={styles.button}>
+        <Text style={styles.buttonText}>Go Back!</Text>
+      </Pressable>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#333333"
+  },
+  headerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 10,
+  },
+  headerText: {
+    paddingRight: 10,
+    paddingLeft: 20,
+    paddingTop: 30,
+    paddingBottom: 10,
+    fontSize: 30,
+    color: '#EDEFEE',
+    textAlign: 'center',
+  },
+  buttonText: {
+    color: 'black',
+    textAlign: 'center',
+    fontSize: 25,
+  },
+  button: {
+    fontSize: 22,
+    padding: 10,
+    marginVertical: 8,
+    margin: 100,
+    backgroundColor: '#EE9972',
+    borderColor: '#EE9972',
+    borderWidth: 2,
+    borderRadius: 50,
+  },
+  regularText: {
+    fontSize: 24,
+    padding: 20,
+    marginVertical: 8,
+    color: '#EDEFEE',
+    textAlign: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 20,
+  },
+});
+
+
